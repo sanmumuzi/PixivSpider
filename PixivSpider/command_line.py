6164ae8240156b503763a648ef1100b61e1e5910
sanmumuzi <332627946@qq.com>
2018-03-27 10:44:41 +0800

    Function: save_json_data_file: modify feature: append data to file.

--- PixivSpider/command_line.py
+++ PixivSpider/command_line.py
@@ -1,13 +1,15 @@
 # -*- coding:utf-8 -*-
 import argparse
-import os
 import json
-
-# import sys
-# sys.path.insert(0, os.path.abspath(os.pardir))  # only to test.
+import os
+from pprint import pprint
 
 from PixivSpider.PixivSpiderApi import *
 from PixivSpider.setting import save_folder
+
+
+# import sys
+# sys.path.insert(0, os.path.abspath(os.pardir))  # only to test.
 
 
 def process_args():  # 使用argparse库分析命令行参数
@@ -18,8 +20,12 @@ def process_args():  # 使用argparse库分析命令行参数
     parser.add_argument('-u', '-account', dest='account', action='store', help='Your Pixiv_username.')
     parser.add_argument('-p', '-password', dest='password', action='store', help='Your Pixiv_password.')
     # parser.add_argument('-pid', '--painter_id', help='Painter ID', required=True)
-    parser.add_argument('-painter_id', dest='painter_id', nargs='*', help='Painter ID')
-    parser.add_argument('-picture_id', dest='picture_id', nargs='*', help='Picture ID')
+    parser.add_argument('-painter_id', dest='painter_id', nargs='*',
+                        help='Painter ID. '
+                             '(If picture_id and painter_id are set at the same time, only picture_id is valid.)')
+    parser.add_argument('-picture_id', dest='picture_id', nargs='*',
+                        help='Picture ID. '
+                             '(If picture_id and painter_id are set at the same time, only picture_id is valid.)')
     parser.add_argument('-dpic', '--download_picture', dest='download_picture', action='store_true',
                         help='download picture')
     parser.add_argument('-picInfo', dest='download_picture_info', action='store_true',
@@ -32,6 +38,8 @@ def process_args():  # 使用argparse库分析命令行参数
     parser.add_argument('-allPic', dest='download_all_picture', action='store_true',
                         help='download all picture of a painter')
     parser.add_argument('-o', '--outpath', dest='outpath', action='store', help='out path')
+    # parser.add_argument('-df', '--dataformat', dest='data_format', action='store',
+    #                     choices={'json', 'python'}, default='json', help='Data storage format')
     # note: only absolute paths are supported at this version
     # And only have a ID (picture id or painter id).
     # ...等将来完善吧...
@@ -43,19 +51,20 @@ def logic_call():
     args = process_args()
     base_args_check(args)  # 参数检查
     if args.outpath is None:
-        args.outpath = save_folder
-    if args.picture_id is not None:
-        for picture_id in args.picture_id:
+        args.outpath = save_folder  # default storage path.
+
+    if args.picture_id is not None:  # picture_id 和 painter_id 只有一个有作用，picture_id优先
+        for picture_id in args.picture_id:  # allow use multiple parameters.
             if args.download_picture:
                 get_a_picture(picture_id, args.outpath, args.account, args.password)
             if args.download_picture_info:
                 picture_info = get_picture_info(picture_id, args.account, args.password)
-                print(picture_info)
+                print_picture_info(picture_info)
                 save_json_data_file(os.path.join(args.outpath, 'picture_info'), picture_info)
             if args.download_painter_info:
                 painter_info = get_painter_info(picture_id=picture_id, account=args.account,
                                                 password=args.password)
-                print(painter_info)
+                print_painter_info(painter_info)
                 save_json_data_file(os.path.join(args.outpath, 'painter_info'), painter_info)
             if args.add_bookmark:
                 add_bookmark(picture_id=picture_id, comment=args.add_comment, tag=args.add_tag,
@@ -63,16 +72,30 @@ def logic_call():
             if args.download_all_picture:
                 get_all_picture_of_painter(picture_id=picture_id, account=args.account,
                                            password=args.password)
-    if args.painter_id is not None:
+    elif args.painter_id is not None:
         for painter_id in args.painter_id:
             if args.download_painter_info:
                 painter_info = get_painter_info(painter_id=painter_id, account=args.account,
                                                 password=args.password)
-                print(painter_info)
+                print_painter_info(painter_info)
                 save_json_data_file(os.path.join(args.outpath, 'painter_info'), painter_info)
             if args.download_all_picture:
                 get_all_picture_of_painter(painter_id=painter_id, account=args.account,
                                            password=args.password)
+
+
+def print_picture_info(picture_info):
+    picture_id, picture_title, picture_introduction, alreadymark = picture_info
+    print('The current picture ID is {}.'.format(picture_id))
+    print('The current picture\'s title is {}.'.format(picture_title))
+    print('The current picture\'s introduction is {}.'.format(picture_introduction))
+    if alreadymark is None:
+        alreadymark = 'The feather is not yet realized.'
+    print('Whether the current picture is bookmarked : {]'.format(alreadymark))
+
+
+def print_painter_info(painter_info):
+    pprint(painter_info)
 
 
 def base_args_check(args):
@@ -91,9 +114,17 @@ def base_args_check(args):
             os.makedirs(args.outpath)
 
 
-def save_json_data_file(filepath, native_data):
-    with open(filepath, 'w') as f:
-        f.write(json.dumps(native_data))
+def save_json_data_file(filepath, native_data):  # Append data to file.
+    try:
+        with open(filepath, 'rt') as f:  # Read the original data in the file
+            pre_data = json.load(f)  # json -> built-in data type
+            pre_data.append(native_data)
+        with open(filepath, 'wt') as p:
+            json.dump(pre_data, p)  # built-in data type -> json
+    except FileNotFoundError as e:
+        temp_list = [native_data]  # Use a list wrapper when add data firstly.
+        with open(filepath, 'wt') as f:
+            json.dump(temp_list, f)
 
 
 if __name__ == '__main__':
